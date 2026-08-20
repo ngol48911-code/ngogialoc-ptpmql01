8fb4fca10b4b26155638cde11702f334ea4ec343
gialoc <ngol48911@gmail.com>
2026-08-20 14:50:46 +0700

    CreateProject

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ngogialoc_ptpmql01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
