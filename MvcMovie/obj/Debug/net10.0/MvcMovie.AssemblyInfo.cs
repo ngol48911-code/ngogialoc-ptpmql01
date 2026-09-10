ce79031bd636f22857148a18598b0cd3bf59d85d
gialoc <ngol48911@gmail.com>
2026-09-10 14:12:43 +0700

    .

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120a376b643e71792bc40c964f703dc53cfe4357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64dd38286c777649e2782cd46f7e81b1aeab58b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
